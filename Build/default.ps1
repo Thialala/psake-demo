cb4befd2e9c5a3544eb835948436652b884327c2
Thialala <obarrypublic@gmail.com>
2015-11-24 00:17:05 +0100

    Integrated unit test

--- Build/default.ps1
+++ Build/default.ps1
@@ -5,6 +5,15 @@
 	$solutionDirectory = (Get-Item $solutionFile).DirectoryName
 	$outputDirectory = "$solutionDirectory\.build"
 	$tempDirectory = "$outputDirectory\temp"
+
+	$publishedNUnitTestsDirectory = "$tempDirectory\_PublisherNUnitTests"
+	$testResultsDirectory = "$outputDirectory\TestResults"
+	$NUnitTestResultsDirectory = "$testResultsDirectory\NUnit"
+	$NUnitExe = ((Get-ChildItem("..\packages\NUnit.Runners*")) | 
+			Select-Object $_.FullName | 
+			Sort-Object $_ | 
+			Select -Last 1).FullName + "\tools\nunit-console-x86.exe"
+
 	$buildConfiguration = "Release"
 	$buildPlatform = "Any CPU"
 }
@@ -29,6 +38,11 @@ task Init -description "Initialises the build by removing previous artifacts and
 		Remove-Item $outputDirectory -Force -Recurse
     }
 
+	# Check that all tools are available
+	Write-Host "Checking that all required tools are available"
+
+	Assert (Test-Path $NUnitExe) "NUnit Console could not be found"
+
 	Write-Host "Creating output directory located at ..\.build"
 	New-Item $outputDirectory -ItemType Directory | Out-Null
 
@@ -49,6 +63,36 @@ task Compile -depends Init `
 	}
 }
 
-task Test -depends Compile, Clean -description "Run the tests" {
+task Test -depends Compile, TestNUnit -description "Run the tests" {
 	Write-Host $testMessage
+}
+
+task TestNUnit -depends Compile `
+			   -description "Run NUnit Tests" {
+	$projects = Get-ChildItem $publishedNUnitTestsDirectory
+
+	if($projects.Count -eq 1)
+	{
+		Write-Host "1 NUnit project has been found:"
+	}
+	else
+	{
+		Write-Host $projects.Count " NUnit projects have been found:"
+	}
+
+	Write-Host ($projects | Select $_.Name)
+
+	# Create the tests results directory if needed
+	if(!(Test-Path $NUnitTestResultsDirectory))
+	{
+		Write-Host "Creating tests results directory at $NUnitTestResultsDirectory"
+		mkdir $NUnitTestResultsDirectory | Out-Null
+	}
+
+	# Get the list of dlls
+	$testAssemblies = $projects | ForEach-Object { $_.FullName + "\" + $_.Name + ".dll" }
+	$testAssembliesParameter = [string]::Join(" ", $testAssemblies)
+
+	Exec { 
+		&$NunitExe $testAssembliesParameter /xml:$NUnitTestResultsDirectory\NUnit.xml /nologo /noshadow }
 }
